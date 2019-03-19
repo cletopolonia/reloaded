7ab1605618aaa6c8863b2277286ba7c8221efc4e
sferraris <simone.ferraris@gfmintegration.it>
2019-03-19 10:54:52 +0100

    - add custom from / to

--- src/main/java/it/dev/cleto/Reloaded.java
+++ src/main/java/it/dev/cleto/Reloaded.java
@@ -14,15 +14,14 @@ public class Reloaded {
 
     public static void main(String[] args) throws Exception {
 
-        // TODO custom from/to
-        // TODO spostare il download.csv nel repo ?
         // TODO trasformare il csv in excel
 
         Utils.banner("Start");
 
-        boolean customON = false;
+        boolean isCustomURL = false;
+        boolean isCustomFromTo = false;
 
-        if (customON) {
+        if (isCustomURL) {
             String url = "https://media.deejay.it/legacy/audio/chiamate_roma_triuno_triuno/20190313.mp3";
             String date = "20190313";
             MP3Show custom = new MP3Show(EShow.CR31, date, url);
@@ -35,6 +34,13 @@ public class Reloaded {
         Date now = nowCal.getTime();
         Date lastDownload = Utils.dateLastDownload();
 
+        if (isCustomFromTo) {
+            String start = "20190313";
+            String stop = "20190313";
+            now = Utils.parseDate(start);
+            lastDownload = Utils.parseDate(stop);
+        }
+
         while (now.after(lastDownload)) {
             Utils.banner("  date: " + Utils.getDateFormat(lastDownload));
             Programming beforeYesterdayProgramming = new Programming(lastDownload);
@@ -43,5 +49,6 @@ public class Reloaded {
         }
 
         Utils.banner("End");
+        System.exit(0);
     }
 }
